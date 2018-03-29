642f56101ecd40b9e9110a79f791ef6ab05e94bb
Anton Rinas <anton.rinas@gmail.com>
2018-03-30 00:24:54 +0300

    БД

--- db/com_project.sql
+++ db/com_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 28 2018 г., 22:00
+-- Время создания: Мар 29 2018 г., 21:24
 -- Версия сервера: 5.7.14
 -- Версия PHP: 7.1.14
 
@@ -30,16 +30,31 @@ CREATE TABLE `comments` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_name` varchar(255) DEFAULT NULL,
   `content` text,
+  `content_changed` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `observers`
+--
+
+CREATE TABLE `observers` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `observer_class_name` text,
+  `event_name` varchar(100) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `comments`
+-- Дамп данных таблицы `observers`
 --
 
-INSERT INTO `comments` (`id`, `user_name`, `content`, `created_at`, `updated_at`) VALUES
-(1, 'Anton Rinas', 'комментарий', '2018-03-28 20:03:54', '2018-03-28 20:03:54');
+INSERT INTO `observers` (`id`, `observer_class_name`, `event_name`, `created_at`, `updated_at`) VALUES
+(1, 'Api\\Observer\\CommentObserver', 'onSubmit', '2018-03-29 21:46:32', '2018-03-29 21:46:34');
 
 --
 -- Индексы сохранённых таблиц
@@ -52,6 +67,12 @@ ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `observers`
+--
+ALTER TABLE `observers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -59,6 +80,11 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT для таблицы `comments`
 --
 ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT для таблицы `observers`
+--
+ALTER TABLE `observers`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
